6eb5ee770ab5be57230bd71c745141b5cfe25f3a
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-07 16:51:31 +0300

    DI Part 1

--- src/main/java/ru/alexsem/springcource/TestSpring_lesson4.java
+++ src/main/java/ru/alexsem/springcource/TestSpring_lesson4.java
@@ -7,8 +7,8 @@ public class TestSpring_lesson4 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
