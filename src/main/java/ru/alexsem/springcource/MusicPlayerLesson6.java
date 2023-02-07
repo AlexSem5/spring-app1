855c1addb5ebd18bee5dad9dd36e82fa670af9d6
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-07 18:56:36 +0300

    DI Part 2

---
+++ src/main/java/ru/alexsem/springcource/MusicPlayerLesson6.java
@@ -0,0 +1,31 @@
+package ru.alexsem.springcource;
+
+public class MusicPlayerLesson6 {
+    private Music music;
+    private String name;
+    private int volume;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getVolume() {
+        return volume;
+    }
+    
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+    
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
