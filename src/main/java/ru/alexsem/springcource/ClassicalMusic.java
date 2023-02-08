456a87dcc0789e647b541724356d83dc207d40b5
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-08 18:33:24 +0300

    Bean Lifecycle

--- src/main/java/ru/alexsem/springcource/ClassicalMusic.java
+++ src/main/java/ru/alexsem/springcource/ClassicalMusic.java
@@ -1,6 +1,18 @@
 package ru.alexsem.springcource;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+    
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+    public void doMyInit() {
+        System.out.println("Initialization");
+    }
+    
+    public void doMyDestroy() {
+        System.out.println("Destruction");
+    }
     
     @Override
     public String getSong() {
--- src/main/java/ru/alexsem/springcource/TestSpring7.java
+++ src/main/java/ru/alexsem/springcource/TestSpring7.java
@@ -7,13 +7,18 @@ public class TestSpring7 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext7.xml"
         );
-        MusicPlayer7 firstMusicPlayer = context.getBean("musicPlayer7", MusicPlayer7.class);
-        MusicPlayer7 secondMusicPlayer = context.getBean("musicPlayer7", MusicPlayer7.class);
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-        firstMusicPlayer.setVolume(70);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer7 firstMusicPlayer = context.getBean("musicPlayer7", MusicPlayer7.class);
+//        MusicPlayer7 secondMusicPlayer = context.getBean("musicPlayer7", MusicPlayer7.class);
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//        firstMusicPlayer.setVolume(70);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+        
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic2.getSong());
         context.close();
     }
 }
