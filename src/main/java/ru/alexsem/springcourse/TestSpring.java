9795afe4007e625640e55c6fa3fc3aa2ca37a53f
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-20 19:29:27 +0300

    Spring IoC container

--- src/main/java/ru/alexsem/springcourse/TestSpring.java
+++ src/main/java/ru/alexsem/springcourse/TestSpring.java
@@ -3,6 +3,12 @@ package ru.alexsem.springcourse;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
+    /**
+     * В методе считываются данные из конфигурационного файла applicationContext.xml
+     * (как создать Bean), создаются бины и кладутся в applicationContext.
+     * Далее бины из него извлекаются.
+     * @param args
+     */
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
